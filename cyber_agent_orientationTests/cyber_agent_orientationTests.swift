9ac143c24962a5c53b583489438d7bd22fdd830b
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2014-08-16 18:36:24 +0900

    Initial Commit

---
+++ cyber_agent_orientationTests/cyber_agent_orientationTests.swift
@@ -0,0 +1,35 @@
+//
+//  cyber_agent_orientationTests.swift
+//  cyber_agent_orientationTests
+//
+//  Created by ucuc on 8/16/14.
+//  Copyright (c) 2014 ucuc. All rights reserved.
+//
+
+import XCTest
+
+class cyber_agent_orientationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
